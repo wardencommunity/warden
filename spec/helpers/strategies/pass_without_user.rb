2b00ff37f19f01a097a59494e5d86375d8024a1f
José Valim <jose.valim@gmail.com>
2010-01-06 13:09:15 +0100

    Update history and version.

--- spec/helpers/strategies/pass_without_user.rb
+++
@@ -1,7 +0,0 @@
-Warden::Strategies.add(:pass_without_user) do
-  def authenticate!
-    request.env['warden.spec.strategies'] ||= []
-    request.env['warden.spec.strategies'] << :pass_without_user
-    success!(nil)
-  end
-end
