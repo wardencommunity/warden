03290577604e64ce7835f1050edff66a17a47fa1
Daniel Neighman <has.sox@gmail.com>
2010-09-23 21:38:33 +1000

    Initial Site

---
+++ config.rb
@@ -0,0 +1,12 @@
+require 'susy'
+require 'fancy-buttons'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "src"
+images_dir = "images"
+javascripts_dir = "javascripts"
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
