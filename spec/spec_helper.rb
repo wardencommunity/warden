31e4ea0fd097f1aa0707225b7555d5f9de90887a
Daniel Neighman <has.sox@gmail.com>
2015-12-23 18:11:13 -0800

    WIP

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ $TESTING=true
 
 $:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 $:.unshift File.expand_path(File.join(File.dirname(__FILE__)))
+require 'bundler/setup'
+
 require 'warden'
 
 require 'rubygems'
