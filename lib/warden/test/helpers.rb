b5179f1d57a961df9b57ef14bbc97e380ac2360c
Annie Caron <annie.caron@cgi.com>
2016-01-25 10:30:33 -0500

    Add mocked warden in test helpers

--- lib/warden/test/helpers.rb
+++ lib/warden/test/helpers.rb
@@ -1,5 +1,7 @@
 # encoding: utf-8
 
+require 'rack'
+
 module Warden
   module Test
     # A collection of test helpers for testing full stack rack applications using Warden
@@ -31,6 +33,61 @@ module Warden
           proxy.logout(*scopes)
         end
       end
+
+      # A helper method that provides the warden object by mocking the env variable.
+      # @api public
+      def warden
+        @warden ||= begin
+          env = env_with_params
+          setup_rack(success_app).call(env)
+          env['warden']
+        end
+      end
+
+      private
+
+      FAILURE_APP = lambda{|e|[401, {"Content-Type" => "text/plain"}, ["You Fail!"]] }
+
+      def env_with_params(path = "/", params = {}, env = {})
+        method = params.delete(:method) || "GET"
+        env = { 'HTTP_VERSION' => '1.1', 'REQUEST_METHOD' => "#{method}" }.merge(env)
+        Rack::MockRequest.env_for("#{path}?#{Rack::Utils.build_query(params)}", env)
+      end
+
+      def setup_rack(app = nil, opts = {}, &block)
+        app ||= block if block_given?
+
+        opts[:failure_app]         ||= failure_app
+        opts[:default_strategies]  ||= [:password]
+        opts[:default_serializers] ||= [:session]
+        blk = opts[:configurator] || proc{}
+
+        Rack::Builder.new do
+          use opts[:session] || Warden::Test::Helpers::Session unless opts[:nil_session]
+          use Warden::Manager, opts, &blk
+          run app
+        end
+      end
+
+      def failure_app
+        Warden::Test::Helpers::FAILURE_APP
+      end
+
+      def success_app
+        lambda{|e| [200, {"Content-Type" => "text/plain"}, ["You Win"]]}
+      end
+
+      class Session
+        attr_accessor :app
+        def initialize(app,configs = {})
+          @app = app
+        end
+
+        def call(e)
+          e['rack.session'] ||= {}
+          @app.call(e)
+        end
+      end # session
     end
   end
 end
